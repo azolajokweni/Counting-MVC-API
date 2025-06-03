d05b16742b16196326494f1540bf2c99c9153e7c
azolajokweni <144328836+azolajokweni@users.noreply.github.com>
2025-06-03 18:49:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,3 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
-builder.services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
